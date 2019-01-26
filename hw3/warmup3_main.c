0a78d3be6ee0407ede3d81ffb99d34ad64945c14
Mahmoud Malcolm Essaid <32280995+malcolmessaid@users.noreply.github.com>
2019-01-26 00:08:32 +0000

    done with content of warmup

--- hw3/warmup3_main.c
+++ hw3/warmup3_main.c
@@ -8,25 +8,43 @@
 
 int main() {
   // Problem 1
-  int board1[3];
-  remove_max(board1, 3);
+  int board1[] = {1, 3, 6, 5, 6, 2, 3};
+  printf("Problem 1 Test\n");
+  printf("%d\n\n", remove_max(board1, 7));
 
   // Problem 2
   char board2[3][3];
+  printf("Problem 2 Test\n");
   init_board(board2);
 
 
   // Problem 3
   char board3[3][3];
+  printf("Problem 3 Test\n");
   place_piece(board3,'A', 1, 1);
 
+ // Problem 4
+ double *a, *p;
+ double area, permi;
+ area = 0.0;
+ permi = 0.0;
+ a = &area;
+ p = &permi;
+ area_and_perimeter(3, 4, a, p);
+ // How do you print a pointer
+ printf("Problem 4 Test\n");
+ printf("%f\n", area);
+ printf("%f\n\n", permi);
 
-// Intrsuctions were confusing. Not sure if we're supposed to call these
-  //
-  // // Problem 4
-  // void area_and_perimeter(double height, double length, double *area, double *perimeter);
-  //
-  // // Problem 5
-  // int remove_max_in_out(int array[], unsigned int *length);
+  // Problem 5
+  int test5[] = {1, 3, 6, 5, 6, 2, 3};
+  unsigned int *len;
+  unsigned int length5 = 7;
+  len = &length5;
+  printf("Problem 5 Test\n");
+  printf("%d\n",remove_max_in_out(test5, len));
+  printf("%d\n\n", length5);
+
+
   return 0;
 }
