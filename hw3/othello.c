01a94d48884bc987c50795b439f7769013911e68
messaid <messaid@dbfdd937-1c43-4170-a353-ed1e29415a3c>
2019-01-29 04:53:32 +0000

    finished othello

--- hw3/othello.c
+++ hw3/othello.c
@@ -10,12 +10,17 @@ int main() {
   init_board(board1);
   char first = 'B';
 
-  print_othello_board(board1);
-  printf("\n");
-  place_piece(board1, first, 2, 3);
-  printf("\n");
-  print_othello_board(board1);
-  //play_othello();
+  //print_othello_board(board1);
+  // printf("\n");
+  // printf("%u\n", place_piece(board1, first, 2, 3));
+  // printf("\n");
+  // print_othello_board(board1);
+//  printf("%u\n", surroundings(board1, 'B', 2, 3, 0, 1)); // THIS WORKS
+  play_othello();
+
+
+//  printf("%u\n", in_line(board1, 'B', 2, 3));
+//  printf("%u\n", flip_pieces(board1, 2, 3));
 
   return 0;
 }
