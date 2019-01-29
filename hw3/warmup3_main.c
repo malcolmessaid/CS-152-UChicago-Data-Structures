c5b51ea463138464abb4534f93e78e3a0ff01b87
Mahmoud Malcolm Essaid <32280995+malcolmessaid@users.noreply.github.com>
2019-01-29 05:41:05 +0000

    almost done

--- hw3/warmup3_main.c
+++ hw3/warmup3_main.c
@@ -27,8 +27,9 @@ unsigned int remove_max_test(int array[], unsigned int length, int expected) {
 /*  init_board_test: tests init_board
  * inputs:
  * 	char board[3][3] - initlized 3 by 3 array.
+ * int expected - expected value
  * outputs:
- *  nothing
+ *  1 if passed. 0 if Failed.
  */
 unsigned int init_board_test(char board[3][3], char expected[3][3]){
   int i,j;
@@ -45,6 +46,39 @@ unsigned int init_board_test(char board[3][3], char expected[3][3]){
   return 1;
 }
 
+
+/*  place_piece_test: tests place_piece
+ * inputs:
+ * 	char board[3][3] -- current board
+ *  char player -- whose move it is
+ * unsigned int row - row to move to
+ * unsigned int col - col to move to
+ * int expected - expected value
+ * outputs:
+ *  1 if passed. 0 if Failed.
+*/
+
+unsigned int place_piece_test(char board[3][3], char player, unsigned int row, unsigned int col, unsigned int expected){
+  place_piece(board, player, row, col);
+  if (board[row][col] == expected) {
+    printf("Test Passed\n");
+    return 1;
+  }
+  else {
+    printf("Test Failed\n");
+    return 0;
+  }
+}
+
+void area_and_perimeter_test(double height, double length, double *area, double *perimeter){
+
+}
+
+// Problem 5
+int remove_max_in_out_test(int array[], unsigned int *length){
+
+}
+
 int main() {
   // Problem 1
   int board1[] = {1, 3, 6, 5, 6, 2, 3};
@@ -52,26 +86,27 @@ int main() {
   int board4[] = {1, 3, 6, 5, 6, 2, 3};
   int board5[] = {1};
   printf("Problem 1 Test\n");
-  printf("%d\n", remove_max(board1, 7));
   remove_max_test(board3, 7, 123);
   remove_max_test(board5, 1, 1);
-  printf("\n");
+  remove_max_test(board4, 6, 6);
 
   // Problem 2
   char board2[3][3];
   char board10[3][3] = {{'*', '*', '*'},
                         {'*', '*', '*'},
                         {'*', '*', '*'}};
+
   printf("Problem 2 Test\n");
   init_board(board2);
   init_board_test(board2, board10);
 
-
-
   // Problem 3
   char board6[3][3];
   printf("Problem 3 Test\n");
-  place_piece(board3,'A', 1, 1);
+  place_piece(board2,'A', 1, 1);
+  place_piece_test(board2,'A', 1, 1, 'A');
+  place_piece_test(board2,'B', 1, 2, 'B');
+  place_piece_test(board2,'C', 2, 0, 'C');
 
  // Problem 4
  double *a, *p;
