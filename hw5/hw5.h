890cf918351613a74461228d90ed16ca5c1dc114
Mahmoud Malcolm Essaid <32280995+malcolmessaid@users.noreply.github.com>
2019-02-12 05:32:06 +0000

    almst done

--- hw5/hw5.h
+++ hw5/hw5.h
@@ -24,8 +24,47 @@ safe_string* make_empty_string();
 safe_string* make_string(char *str);
 
 
+/* safe_str_cpy
+ * Searches stycpy for safe_string. Allocates new memory if necessary, does
+ * not if not not necessary
+ * input:
+ *    safe_string *dest - pointer to be copied into
+ *    safe_string *src - string to be copied over
+ * output: void
+*/
 void safe_str_cpy(safe_string *dest, safe_string *src);
+
+/* safe_str_cat
+ * Searches concatanates safe_string. Allocates new memory if necessary.
+ * input:
+ *    safe_string *dest - string to be copied to
+ *    safe_string *src - string to be copied over
+ * output: void
+*/
 void safe_str_cat(safe_string *dest, safe_string *src);
+
+
+/* safe_strchr
+ * Searches safe_string for char c.
+ * input:
+ *    safe_string *s -- String to be searched
+ *    char c - char to look for
+ * output: char* - Returns pointer to first instance of c. NULL if not in string
+*/
+char *safe_strchr(safe_string *s, char c);
+
+/* safe_strcmp
+ * Compares safestring s1 and s2. Compares alphabeticly, with A being smaller
+ * a.
+ * input:
+ *    safe_string *s1  -- safe string 1
+ *    safe_string *s2  -- safe string 2
+ * output: int - Returns -1 if s1 is smaller, 0 if equal. 1 if s1 is greater.
+*/
+int safe_strcmp(safe_string *s1, safe_string *s2);
+
+
+
 
 
 
