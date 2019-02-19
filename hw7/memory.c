7dc8c4e5fb30bea5eebf1e1e5692785cde55481f
messaid <messaid@dbfdd937-1c43-4170-a353-ed1e29415a3c>
2019-02-19 21:01:06 +0000

    skeleton

---
+++ hw7/memory.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "memory.h"
+
+/* memory_new
+ * create a new memory struct, initialze its address and size
+ */
+memory* memory_new(unsigned int addr, unsigned int size){
+  memory* m = (memory*)malloc(sizeof(memory));
+  m->addr = addr;
+  m->size = size;
+  return m;
+}
+
+/* free the dynamically allocated memory struct
+ */
+void memory_free(void* p){
+  memory* m = (memory*)p;
+  free(m);
+}
+
+/* compare two memory variables x and y by address 
+ * if x is less than y, return -1
+ * if x is greater than y, return 1
+ * if they are equal, return 0
+ */
+int memory_addr_cmp(const void* x, const void* y){
+  //TODO
+  return 0;
+}
+
+/* compare two memory variables x and y by size 
+ * if x is less than y, return -1
+ * if x is greater than y, return 1
+ * if they are equal, return 0
+ */
+int memory_size_cmp(const void* x, const void* y){
+  //TODO
+  return 0;
+}
+
+/* print the memory address and size
+ */
+void memory_print(void* data){
+  if (data == NULL) return;
+  memory* m = (memory*)data;
+  printf("address: %u, size: %u\n", m->addr, m->size);
+}
