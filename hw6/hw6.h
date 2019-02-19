b92f37ee3d7709e0a39b9b4796e45d3e8c08126f
messaid <messaid@dbfdd937-1c43-4170-a353-ed1e29415a3c>
2019-02-19 03:17:16 +0000

    testing

--- hw6/hw6.h
+++ hw6/hw6.h
@@ -25,16 +25,39 @@ struct _node{
 };
 
 extern char *category_strings[];
+
+/* safe_str_concat
+ *  allocates a brand new string that will hold both str1 and str2.
+ * Then it copies both strings over, str1 at the front and str2 at the back.
+ * input:
+ *    char *str1 - string to concatanate
+ *    char *str2 - string to concatanate
+ * output: int - returns a pointer to that new string.
+ */
 void print_product(product_info *g, FILE *fp);
 void print_list(node *head, FILE *fp);
 
 
+
+/* safe_str_concat
+ *  allocates a brand new string that will hold both str1 and str2.
+ * Then it copies both strings over, str1 at the front and str2 at the back.
+ * input:
+ *    char *str1 - string to concatanate
+ *    char *str2 - string to concatanate
+ * output: int - returns a pointer to that new string.
+ */
 product_info *create_product(char *name, char *category, uint id,
 				uint current, uint mn, uint mx);
 
 
 node* insert_head(node *head, product_info *pinfo);
 
+product_info *find(node *head, uint pID);
+
+
+node *add_sorted_productID(product_info *pinfo, node *head);
+node *add_sorted_category_ID(product_info *pinfo, node *head);
 void record_restocked_single(node *head, uint pID);
 
 void product_sold(node *head, uint pID);
