a87c89e873ca775ee98ec9a546c07733dcc55fd1
messaid <messaid@dbfdd937-1c43-4170-a353-ed1e29415a3c>
2019-03-12 04:29:58 +0000

    almost done

--- hw8/memory.c
+++ hw8/memory.c
@@ -118,10 +118,7 @@ void *split_memory(memory* data, unsigned int size_desired) {
 
 
   data->size = data->size - size_desired - 8;
-  // THis might be plus 16
 
-
-  // Points to the beginning of the usable chunk
   return add_to_address(data->addr, data->size + 8);
 
 }
@@ -146,42 +143,14 @@ memory *merge_memory(memory *first, memory *second) {
   unsigned int* s_size;
   f_size = add_to_address(f_add, -8);
   s_size = add_to_address(s_add, -8);
-  // printf("%d\n", first->size);
-  // printf("%d\n", second->size);
-  //
-  // printf("%ld\n", f_add - s_add);
-  //
-  // printf("merge 5 plus 120 %p\n", add_to_address(s_add, 120));
-  // printf("merge 5 plus 128 %p\n", add_to_address(s_add, 128));
-  // printf("merge 5 plus 136 %p\n", add_to_address(s_add, 136));
-
-  //
-  // printf(" %d\n", 48 + 16);
-  // printf("second size %d\n", second->size);
-  //   printf("first size %d\n", first->size);
-  // printf("Second Address Plus Size %p\n", add_to_address(s_add, 8 + second->size));
-  // printf("First address %p\n", f_add);
-  //
-  // printf("First Address Plus Size %p\n", add_to_address(f_add, 8 + first->size));
-  // printf("Second address %p\n", add_to_address(s_add , 8));
 
 
   if ((add_to_address(f_add, 8 + first->size) == s_add) ||
       add_to_address(s_add, 8 + second->size) == f_add){
 
-  //  printf("%s\n", );
-  // printf("second size %d\n", second->size);
-  //   printf("first size %d\n", first->size);
     first->size = (8 + first->size + second->size);
     *f_size = first->size;
 
-// Delete this based on decison
-// Please Note: In the directions it says to free uneeded structs here. But
-// from Piazza and what I think make more sense, I will free the structs in
-// compact_memory instead;
-
-
-// You need to delete based on address not size
     bst_delete(avail_mem, second);
 
     free(second);
